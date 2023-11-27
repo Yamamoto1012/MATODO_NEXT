30e3587b7efd779c9da9c19e7fb2ed9976bd7f4d
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-11-27 16:15:44 +0900

    プロフィールページ

--- front/app/Profile/page.jsx
+++ front/app/Profile/page.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase";
+import { collection, doc, getDoc, query, where } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
-const page = () => {
+export default function ProfilePage() {
+  //userテーブルからcurrentUserの情報を取得
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        // ログインユーザーがいる場合、Firestoreからデータを取得
+        const userRef = doc(db, "users", currentUser.uid);
+        getDoc(userRef).then((docSnap) => {
+          if (docSnap.exists()) {
+            setUser(docSnap.data());
+            console.log("ユーザーデータを取得しました");
+          } else {
+            console.log("ユーザーデータが見つかりません");
+          }
+        });
+      } else {
+        // ユーザーがログアウトしている場合
+        setUser(null);
+      }
+    });
+    // コンポーネントのアンマウント時にリスナーを解除
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignOut = () => {
+    auth.signOut();
+  };
+
   return (
-    <div>page</div>
-  )
+    <>
+      <h1>プロフィール</h1>
+      <div>
+        <p>名前</p>
+        {/* currentUserのnameを表示 */}
+        <p>{user ? user.name : "読み込み中..."}</p>
+        <button onClick={handleSignOut}>ログアウト</button>
+      </div>
+    </>
+  );
 }
-
-export default page
