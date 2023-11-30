c588fe4d594b368d474d67bb2bc19a0d3eecdd18
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-12-01 00:33:18 +0900

    エラー解消

--- front/ui/Days.jsx
+++ front/ui/Days.jsx
@@ -1,14 +1,20 @@
-"use client";
+import React, { useEffect, useState } from 'react';
+
 export default function Days() {
-  const today = new Date();
-  const date = today.getDate();
-  const day = today.toLocaleString("en-US", { weekday: "short" });
+  const [date, setDate] = useState(null);
+  const [day, setDay] = useState(null);
+
+  useEffect(() => {
+    const today = new Date();
+    setDate(today.getDate());
+    setDay(today.toLocaleString("en-US", { weekday: "short" }));
+  }, []);
 
   return (
     <div className="w-40 h-40 p-7 bg-[#00ADB5] rounded-2xl flex-col justify-center items-center gap-2.5 inline-flex">
       <div className="text-[50px] text-center" style={{ color: "#FFFFFF" }}>
-        <div class="text-white text-4xl font-bold">{day}</div>
-        <div class="text-white text-4xl font-bold pt-2">{date}</div>
+        {day && <div className="text-white text-4xl font-bold">{day}</div>}
+        {date && <div className="text-white text-4xl font-bold pt-2">{date}</div>}
       </div>
     </div>
   );
