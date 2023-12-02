d345c4cb674180d4cc9a2949379f0ed6fa770c11
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2023-12-02 14:25:38 +0900

    理想の動作になった

--- front/app/(auth)/CreateProfile/page.jsx
+++ front/app/(auth)/CreateProfile/page.jsx
@@ -3,15 +3,16 @@ import { useState } from "react";
 import { auth, db, storage } from "../../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { useRouter } from "next/navigation";
 
-export default function page() {
+export default function Page() {
   const [name, setName] = useState("");
   const [position, setPosition] = useState("");
   const [icon, setIcon] = useState(null);
+  const router = useRouter();
 
   const handleIconChange = (e) => {
     if (e.target.files.length > 0) {
-      //アイコン画像のファイルを取得
       const file = e.target.files[0];
       setIcon(file);
     }
@@ -28,7 +29,6 @@ export default function page() {
     let iconUrl = "";
 
     if (icon) {
-      //　画像をFirebase Storageにアップロード
       const iconRef = ref(storage, `icons/${userId}/${icon.name}`);
       await uploadBytes(iconRef, icon);
       iconUrl = await getDownloadURL(iconRef);
@@ -37,39 +37,98 @@ export default function page() {
     const userRef = doc(db, "users", userId);
 
     try {
-      //FireStoreにユーザー情報を保存
       await setDoc(userRef, {
         name: name,
-        positsion: position,
+        position: position,
         iconUrl: iconUrl,
       });
-
       console.log("プロフィールが作成されました");
+      router.push("/");
     } catch (error) {
       console.error("エラーが発生しました", error);
     }
+  };
 
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
+  // コンポーネントのレンダリング部分
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-xl shadow-md max-w-2xl w-full mx-4"
+      >
+        <div className="mb-6">
+          <div className="flex items-center h-4 w-full">
+            <p
+              className="text-gray-700 text-sm font-medium mb-2"
+              htmlFor="email"
+            >
+              ユーザー名
+            </p>
+            <p className="text-red-500 text-[12px] mb-2">(必須)</p>
+          </div>
           <input
+            id="name"
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="コンスタンチノープル"
+            placeholder="dmn大郎"
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            required
           />
+        </div>
+        <div className="mb-6">
+          <div className="flex items-center h-4 w-full">
+            <p className="text-black text-sm font-medium mb-2" htmlFor="email">
+              役職
+            </p>
+            <p className="text-red-500 text-[12px] mb-2">(必須)</p>
+          </div>
           <input
+            id="position"
             type="text"
             value={position}
             onChange={(e) => setPosition(e.target.value)}
-            placeholder="学生"
+            placeholder="役職"
+            className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm transition-transform duration-150 ease-in-out transform hover:scale-105"
+            required
           />
-          {/* アイコン画像を追加 */}
-          <input type="file" accept="image/*" onChange={handleIconChange} />
+        </div>
 
-          <button type="submit">アカウント作成</button>
-        </form>
-      </>
-    );
-  };
+        <div className="mb-6">
+          <label
+            htmlFor="icon"
+            className="block text-sm font-medium text-gray-700"
+          >
+            アイコン (任意)
+          </label>
+          <div className="mt-1 flex items-center">
+            <span className="rounded-md border border-gray-300 shadow-sm bg-white py-2 px-3 text-sm leading-4 text-gray-700">
+              {icon ? icon.name : "アイコンを選択"}
+            </span>
+            <button
+              type="button"
+              className="ml-3 bg-[#00ADB5] text-white font-bold py-2 px-4 rounded-xl"
+              onClick={() => document.getElementById("icon").click()}
+            >
+              アップロード
+            </button>
+            <input
+              id="icon"
+              type="file"
+              accept="image/*"
+              onChange={handleIconChange}
+              className="hidden"
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-[#00ADB5] text-white font-bold py-2 px-4 rounded-xl transition-transform duration-150 ease-in-out transform hover:scale-105"
+        >
+          アカウント作成
+        </button>
+      </form>
+    </div>
+  );
 }
--- front/app/(auth)/Login/page.jsx
+++ front/app/(auth)/Login/page.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import Link from "next/link";
 
-
 //既存のユーザーでログイン
 export default function page() {
   const [email, setEmail] = useState("");
@@ -39,21 +38,22 @@ export default function page() {
 
   return (
     <>
-      <form onSubmit={handleLogin} className="bg-gray-100 h-auto p-16 rounded-lg shadow-md max-w-lg mx-auto my-10">
+      <form
+        onSubmit={handleLogin}
+        className="bg-gray-100 h-auto p-16 rounded-lg shadow-md max-w-lg mx-auto my-10"
+      >
         <div className="mb-4">
           <div className="flex items-center h-4 w-full">
             <p className="text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                メールアドレス 
+              メールアドレス
             </p>
-            <p className="text-red-500 text-[12px] mb-2">
-              (必須)
-            </p>
+            <p className="text-red-500 text-[12px] mb-2">(必須)</p>
           </div>
-          
-          <input 
+
+          <input
             id="email"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            type="email" 
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="sample@sample.com"
@@ -63,16 +63,16 @@ export default function page() {
 
         <div className="mb-4 pt-4">
           <div className="flex items-center h-4 w-full">
-              <p className="text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                  パスワード
-              </p>
-              <p className="text-red-500 text-[12px] mb-2">
-                (必須)
-              </p>
+            <p className="text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              パスワード
+            </p>
+            <p className="text-red-500 text-[12px] mb-2">(必須)</p>
           </div>
 
-          <p className="text-[12px] text-red-500">パスワードは半角英数16文字以上で入力してください</p>
-          <input 
+          <p className="text-[12px] text-red-500">
+            パスワードは半角英数16文字以上で入力してください
+          </p>
+          <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             type="password"
             value={password}
@@ -82,14 +82,14 @@ export default function page() {
           />
         </div>
 
-        <button type="submit" className="w-full bg-[#00ADB5] text-white font-bold py-2 px-4 rounded-xl transition-transform duration-150 ease-in-out transform hover:scale-105">
-          <Link href={"/"}>
-            ログイン
-          </Link>
+        <button
+          type="submit"
+          className="w-full bg-[#00ADB5] text-white font-bold py-2 px-4 rounded-xl transition-transform duration-150 ease-in-out transform hover:scale-105"
+        >
+          <Link href={"/"}>ログイン</Link>
         </button>
       </form>
       {error && <p className="text-red-500 text-center mt-4">{error}</p>}
     </>
   );
-  
 }
