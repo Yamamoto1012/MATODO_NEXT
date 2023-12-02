be044637915e8f22a82ae078f18a12ee4ddddc1e
Yukkin395 <c1254703@st.kanazawa-it.ac.jp>
2023-12-02 17:12:20 +0900

    関数の追加

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from bs4 import BeautifulSoup
 import requests
+import time
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+
 
 # 履修中の科目情報を取得
 def kamoku_info():
@@ -17,19 +22,43 @@ def kamoku_info():
         unit = [target.get('data-risyu'), target.get('data-nendo')]
         if unit not in data:
             data.append(unit)
+
     
     return data
 
 
-def scrape_gakupo():
+# 科目のページに入る
+def enter_kamoku():
+
+    kamoku_info = {
+        "OPEN_YEAR": "2023",  # 年度
+        "RISYU_CODE":"G224012006"  # 科目コード(?)
+    }
+
+    request_url = "https://mars41.mars.kanazawa-it.ac.jp/eSylla/eSRefSummaryPortalAuth"
+    #m_res = session.post(request_url, data=kamoku_info)
+    m_res = session.post(request_url, params=kamoku_info, verify=False)
     
-    # ターゲットのURLを指定
+    # エラーチェック：HTTPエラーステータスがあれば例外を発生させる
+    m_res.raise_for_status()
+
+    return m_res
+
+
+#メイン関数
+def main_scrape():
+    
+    #2 環境変数の呼び出し
+    uid = os.environ.get("uid")
+    pw = os.environ.get("pw")
+
+    # セッションを開始
     url = "https://navi.mars.kanazawa-it.ac.jp/portal/student"
-    
-    # セッションを開始して、指定したURLにGETリクエストを送信
+
+    global session
     session = requests.session()
     response = session.get(url)
-    
+
     soup = BeautifulSoup(response.text, 'html.parser')
     
     # ログインフォームの情報を取得
@@ -41,16 +70,31 @@ def scrape_gakupo():
     # ログインに必要な情報
     loginfo = {
         "_csrf": authenticity,
-        "uid": "your_id",  # ここに学籍番号を入力
-        "pw":"your_password"  # ここにパスワードを入力
+        "uid": uid,  # ここに学籍番号を入力
+        "pw": pw  # ここにパスワードを入力
     }
     
-    # ログイン
+    # ログイン用のURLを指定し、POSTリクエストを送信
     login_url = "https://navi.mars.kanazawa-it.ac.jp/portal/student/inKITP0000001Login" #エンドポイント設定
     res = session.post(login_url, data=loginfo)
-    res.raise_for_status() #エラー時に例外発生
     
-    return res.text
+    time.sleep(1)
+
+    # エラーチェック：HTTPエラーステータスがあれば例外を発生させる
+    #res.raise_for_status()
+    
+
+    #履修中の科目情報を取得する関数呼びだし
+    #data = kamoku_info()
+    #time.sleep(1)
+    
+    
+    # 科目のページに入る関数
+    data2 = enter_kamoku()
+    time.sleep(1)
+    
+    return data2.text
+
 
 
 @app.route("/")
