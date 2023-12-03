44e9888752f3ee2e308908e358ad2924ded7c97d
Yukkin395 <c1254703@st.kanazawa-it.ac.jp>
2023-12-03 13:59:14 +0900

    見た目を整える

--- front/ui/ScrapingButton.jsx
+++ front/ui/ScrapingButton.jsx
@@ -6,14 +6,12 @@ export default function ScrapingButton() {
   };
 
   return (
-    <div className="flex justify-center items-center mt-4">
-      <button 
-        onClick={handleScraping} 
-        className="bg-[#2B2D42] hover:bg-[#3a3f5a] flex items-center px-4 py-2 rounded-lg shadow-lg transition-all duration-300 ease-in-out cursor-pointer hover:scale-105"
-      >
-        <LeakAddIcon className="text-[#00ADB5] mr-2" />
-        <span className="text-sm text-white font-medium">スクレイピング</span>
-      </button>
+    <div className="pb-2">
+      <div className="flex bg-[#2B2D42] px-3 py-2 rounded-2xl items-center space-x-3 shadow-xl transition-transform duration-300 ease-in-out cursor-pointer hover:scale-110">
+          {handleScraping}
+          <LeakAddIcon className="text-[#00ADB5] mr-2" />
+          <span className="text-sm text-white font-medium">スクレイピング</span>
+      </div>
     </div>
   );
 }
