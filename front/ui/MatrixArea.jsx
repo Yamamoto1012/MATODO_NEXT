a245e93fc54f3988b49cca9f7fc2bcec3a6f9834
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-02-05 22:24:50 +0900

    リファクタリング

--- front/ui/MatrixArea.jsx
+++ front/ui/MatrixArea.jsx
@@ -1,11 +1,47 @@
-"use client";
 import { useEffect, useState } from "react";
 import { auth, db } from "../app/firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { Cached } from "@mui/icons-material";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"; // 追加
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp"; // 追加
+
+function TaskCategory({ title, tasks, bgColorFrom, bgColorTo }) {
+  const [showAll, setShowAll] = useState(false);
+
+  return (
+    <div
+      className={`bg-gradient-to-tr from-${bgColorFrom} to-${bgColorTo} flex flex-col justify-center items-center rounded-lg shadow-md overflow-hidden p-2 transform transition-transform duration-300 ease-in-out hover:scale-105`}
+    >
+      <p className="text-white text-2xl font-semibold mb-2">{title}</p>
+      {tasks.slice(0, showAll ? tasks.length : 4).map((task) => (
+        <div
+          key={task.id}
+          className="bg-[#2B2D42] text-white text-base px-3 py-2 rounded-lg shadow m-1"
+        >
+          {task.title}
+        </div>
+      ))}
+      {tasks.length > 4 && (
+        <button
+          onClick={() => setShowAll(!showAll)}
+          className="text-white mt-2 hover:underline"
+        >
+          {showAll ? (
+            <>
+              <ArrowDropUpIcon /> 折りたたむ
+            </>
+          ) : (
+            <>
+              <ArrowDropDownIcon /> もっと見る
+            </>
+          )}
+        </button>
+      )}
+    </div>
+  );
+}
 
 export function MatrixArea() {
-  // firebaseのtasksテーブルからタスクを取得
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -15,8 +51,8 @@ export function MatrixArea() {
           const tasksCollectionRef = collection(db, "tasks");
           const q = query(
             tasksCollectionRef,
-            where("userId", "==", user.uid), // ユーザーIDに基づくフィルタリング
-            where("isDone", "==", false) // isDoneがfalseのもののみ取得
+            where("userId", "==", user.uid),
+            where("isDone", "==", false)
           );
           const querySnapshot = await getDocs(q);
           const tasksData = querySnapshot.docs.map((doc) => ({
@@ -32,88 +68,50 @@ export function MatrixArea() {
     return () => unsubscribe();
   }, []);
 
-  // タスクをそれぞれのカテゴリに分ける
-  const urgentImportantTasks = tasks.filter(
-    (task) => task.urgency === "high" && task.importance === "high"
-  );
-  const urgentNotImportantTasks = tasks.filter(
-    (task) => task.urgency === "high" && task.importance === "low"
-  );
-  const notUrgentImportantTasks = tasks.filter(
-    (task) => task.urgency === "low" && task.importance === "high"
-  );
-  const notUrgentNotImportantTasks = tasks.filter(
-    (task) => task.urgency === "low" && task.importance === "low"
-  );
-  const handleReload = () => {
-    window.location.reload();
-  };
+  const categories = [
+    {
+      title: "緊急ではないが重要",
+      filter: (task) => task.urgency === "low" && task.importance === "high",
+      bgColorFrom: "green-500",
+      bgColorTo: "green-300",
+    },
+    {
+      title: "緊急で重要",
+      filter: (task) => task.urgency === "high" && task.importance === "high",
+      bgColorFrom: "red-500",
+      bgColorTo: "pink-400",
+    },
+    {
+      title: "緊急でも重要でもない",
+      filter: (task) => task.urgency === "low" && task.importance === "low",
+      bgColorFrom: "gray-500",
+      bgColorTo: "gray-300",
+    },
+    {
+      title: "緊急だが重要ではない",
+      filter: (task) => task.urgency === "high" && task.importance === "low",
+      bgColorFrom: "blue-500",
+      bgColorTo: "blue-300",
+    },
+  ];
 
   return (
-    <div className="grid grid-cols-2 grid-rows-2 gap-4 p-6 mx-auto mt-12 rounded-xl shadow-xl w-[600px] h-[400px] md:w-[725px] md:h-[525px] lg:w-[800px] lg:h-[600px]">
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 p-6 mx-auto mt-12 rounded-xl shadow-xl w-[600px] h-auto md:w-[725px] lg:w-[800px]">
       <button
-        onClick={handleReload}
+        onClick={() => window.location.reload()}
         className="absolute top-5 left-5 bg-gray-200 p-2 rounded-full shadow hover:bg-gray-300"
       >
         <Cached />
       </button>
-      {/* 緊急ではないが重要 */}
-      <div className="bg-gradient-to-tr from-green-500 to-green-300 flex flex-col justify-center items-center rounded-lg shadow-md overflow-y-auto p-2 transform transition-transform duration-300 ease-in-out hover:scale-105">
-        <p className="text-white text-2xl font-semibold mb-2">
-          緊急ではないが重要
-        </p>
-        {notUrgentImportantTasks.map((task) => (
-          <div
-            key={task.id}
-            className="bg-[#2B2D42] text-white text-base px-3 py-2 rounded-lg shadow m-1"
-          >
-            {task.title}
-          </div>
-        ))}
-      </div>
-
-      {/* 緊急で重要 */}
-      <div className="bg-gradient-to-tr from-red-500 to-pink-400 flex flex-col justify-center items-center rounded-lg shadow-md overflow-y-auto p-2 transform transition-transform duration-300 ease-in-out hover:scale-105">
-        <p className="text-white text-2xl font-semibold mb-2">緊急で重要</p>
-        {urgentImportantTasks.map((task) => (
-          <div
-            key={task.id}
-            className="bg-[#2B2D42] text-white text-base px-3 py-2 rounded-lg shadow m-1"
-          >
-            {task.title}
-          </div>
-        ))}
-      </div>
-
-      {/* 緊急でも重要でもない */}
-      <div className="bg-gradient-to-tr from-gray-500 to-gray-300 flex flex-col justify-center items-center rounded-lg shadow-md overflow-y-auto p-2 transform transition-transform duration-300 ease-in-out hover:scale-105">
-        <p className="text-white text-2xl font-semibold mb-2">
-          緊急でも重要でもない
-        </p>
-        {notUrgentNotImportantTasks.map((task) => (
-          <div
-            key={task.id}
-            className="bg-[#2B2D42] text-white text-base px-3 py-2 rounded-lg shadow m-1"
-          >
-            {task.title}
-          </div>
-        ))}
-      </div>
-
-      {/* 緊急だが重要ではない */}
-      <div className="bg-gradient-to-tr from-blue-500 to-blue-300 flex flex-col justify-center items-center rounded-lg shadow-md overflow-y-auto p-2 transform transition-transform duration-300 ease-in-out hover:scale-105">
-        <p className="text-white text-2xl font-semibold mb-2">
-          緊急だが重要ではない
-        </p>
-        {urgentNotImportantTasks.map((task) => (
-          <div
-            key={task.id}
-            className="bg-[#2B2D42] text-white text-base px-3 py-2 rounded-lg shadow m-1"
-          >
-            {task.title}
-          </div>
-        ))}
-      </div>
+      {categories.map((category) => (
+        <TaskCategory
+          key={category.title}
+          title={category.title}
+          tasks={tasks.filter(category.filter)}
+          bgColorFrom={category.bgColorFrom}
+          bgColorTo={category.bgColorTo}
+        />
+      ))}
     </div>
   );
 }
