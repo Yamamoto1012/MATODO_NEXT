76ed4fc6da025287daa447a114b5e7ad09dace4d
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2024-06-13 20:09:35 +0900

    niconico風のコメント機能

--- front/ui/NicoNico.jsx
+++ front/ui/NicoNico.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './NicoNico.css';
 
 export default function NicoNico({ comment }) {
--- front/ui/TaskList.jsx
+++ front/ui/TaskList.jsx
@@ -4,7 +4,6 @@ import { auth, db } from "../app/firebase";
 import {
   query,
   collection,
-  onSnapshot,
   doc,
   updateDoc,
   deleteDoc,
@@ -13,14 +12,15 @@ import {
 } from "firebase/firestore";
 import DatePicker from "react-datepicker";
 import TaskCard from "./TaskCard"; // TaskCardコンポーネントの実装は未定義
-import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import "react-datepicker/dist/react-datepicker.css";
+import NicoNico from "./NicoNico";
 
 export default function TaskList() {
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState(null);
   const [showSidebar, setShowSidebar] = useState(false);
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -54,6 +54,13 @@ export default function TaskList() {
     setShowSidebar(false);
   };
 
+  const handleNicoNico = (comment) => {
+    setComment(comment);
+    setTimeout(() => {
+      setComment("");
+    }, 4000);
+  }
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setSelectedTask((prev) => ({ ...prev, [name]: value }));
@@ -68,6 +75,7 @@ export default function TaskList() {
       const taskRef = doc(db, "tasks", selectedTask.id);
       try {
         await updateDoc(taskRef, updates);
+        handleNicoNico("タスクを更新しました");
         // 更新後のタスクリストをフェッチする必要がある場合は、ここでfetchTasks()を呼び出します。
       } catch (error) {
         console.error("Error updating task:", error);
@@ -166,6 +174,7 @@ export default function TaskList() {
           </div>
         </div>
       )}
+      <NicoNico comment={comment}/>
     </div>
   );
 }
