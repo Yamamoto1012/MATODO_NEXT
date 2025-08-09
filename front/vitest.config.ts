ed98c9657c9b40fb5d47f2c3e506ae03a2e3ebd0
Yamamoto1012 <koki.yamamoto03@gmail.com>
2025-08-09 17:05:24 +0900

    fix:  不要な記述の削除

--- front/vitest.config.ts
+++ front/vitest.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [],
   test: {
     globals: true,
     environment: 'jsdom',
